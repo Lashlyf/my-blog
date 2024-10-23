3d1f0eb2d8c9707aa1f41f68779024569fa122ed
Lashlyf <ladeshamwanza23@gmail.com>
2024-10-23 12:21:28 +0300

    setting up schema

---
+++ studio-blog/schemaTypes/blog.ts
@@ -0,0 +1,36 @@
+import {defineField, defineType} from 'sanity'
+
+export const postType = defineType({
+  name: 'blog',
+  title: 'Blog',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      type: 'string',
+      title:'Title of blog article',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {source: 'title'},
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'publishedAt',
+      type: 'datetime',
+      initialValue: () => new Date().toISOString(),
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
+    }),
+    defineField({
+      name: 'body',
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+  ],
+})
