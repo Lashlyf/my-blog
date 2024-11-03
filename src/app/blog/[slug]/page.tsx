5d1ee6d26026f7dbab36665a905ebb49d43f9d53
Lashlyf <ladeshamwanza23@gmail.com>
2024-11-03 11:18:03 +0300

    Fetching individual blog data

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,8 +1,34 @@
-export default function BlogArticle({params}:{
+import { fullBlog } from "@/app/lib/interface";
+import { client, urlFor } from "@/app/lib/sanity";
+import Image from 'next/image'
+
+async function getData(slug:string){
+    const query = `
+    *[_type == "blog" && slug.current =='${slug}']
+    {
+        "current": slug.current,
+        title,image
+    }[0]`;
+
+    const data =await client.fetch(query);
+    return data;
+}
+
+
+
+export default async function BlogArticle({params}:{
     params:{
         slug:string;
     }}){
+        const data: fullBlog = await getData(params.slug);
+       
     return(
-        <h1>{params.slug}</h1>
+        <div className="mt-6">
+            <h1>
+                <span className="mt-2 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl">{data.title}</span>
+            </h1>
+            <Image src={urlFor(data.image).url()} alt="Default image" height={800} width={800} className="mt-4"/>
+        </div>
+
     )
 }
--- src/app/lib/interface.ts
+++ src/app/lib/interface.ts
@@ -1,7 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export interface simpleBlogCard{
     title:string;
     current:string;
     publishedAt:string;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    image:any;
+}
+
+export interface fullBlog{
+    current:string;
+    title:string;
     image:any;
 }
